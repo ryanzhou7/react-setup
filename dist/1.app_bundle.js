f05c6b95515655158c848eb827e478afaae8ae38
Ryan Zhou <ryanzhou7@gmail.com>
2020-04-01 20:56:18 -0700

    remove dist gitignore

---
+++ dist/1.app_bundle.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[1],{28:function(n,e,t){"use strict";t.r(e),t.d(e,"default",(function(){return r}));var i=t(0),a=t.n(i);function r(){return a.a.createElement("div",null," This was dynamically imported")}}}]);
